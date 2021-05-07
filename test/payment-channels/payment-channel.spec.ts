8749758d754c068537b390438d6eac8770628127
rolandoandrade <rolandoandradefernandez@gmail.com>
2021-05-07 12:46:13 -0400

    Testing sign voucher

--- test/payment-channels/payment-channel.spec.ts
+++ test/payment-channels/payment-channel.spec.ts
@@ -1,13 +1,13 @@
 import { PaymentChannel } from "../../src/payment-channels/payment-channel";
 import * as wasmSigningTools from "@blits-labs/filecoin-signing-tools/nodejs";
-import {CodeCID, INIT_ACTOR, InitMethod} from "../../src/core/types/types";
-import {serialize} from "v8";
+import {CodeCID} from "../../src/core/types/types";
 import BigNumber from "bignumber.js";
 import blake2b from 'blake2b'
 
 describe("payment channels", () => {
     let paymentChannel: PaymentChannel;
     const from = "t1vwxualsf6gx5jjl2fp7zh7gy6ailk4hnwgkroci";
+    const privateKey = "b144cf14dbd413aaefaa4658bca06733aa33386e651ab9816954807c74517bf1";
     const to = "t1aexhfgaaowzz2wryy7b6q5y3zs7tjhybfmqetta";
     const paychAddress = "t01312"
 
@@ -58,7 +58,20 @@ describe("payment channels", () => {
             .update(bSecret)
             .digest('hex');
         const wasmResult = wasmSigningTools.createVoucher(paychAddress, "0","0", secretPreImage, "10", "0", "0", "0");
-        const result = await paymentChannel.createVoucher(paychAddress, 0,0, secretPreImage, new BigNumber(10), 0, 0, 0);
+        const result = paymentChannel.createVoucher(paychAddress, 0,0, secretPreImage, new BigNumber(10), 0, 0, 0);
+        expect(result).toEqual(wasmResult);
+    });
+
+    it("should sign voucher voucher", async () => {
+        const bSecret = Buffer.from("secret");
+        const secretPreImage = blake2b(new Uint8Array(32).length)
+            .update(bSecret)
+            .digest('hex');
+        const voucher = await paymentChannel.createVoucher(paychAddress, 0,0, secretPreImage, new BigNumber(10), 0, 0, 0);
+
+        const wasmResult = wasmSigningTools.signVoucher(voucher, Buffer.from(privateKey, "hex"));
+        const result = paymentChannel.signVoucher(voucher, privateKey);
+
         expect(result).toEqual(wasmResult);
     });
 });
