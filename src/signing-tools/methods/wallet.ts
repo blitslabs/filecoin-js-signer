2d31264ec0be5f6fb5722981e81227fe34628fb1
blits-labs <defi@blits.net>
2021-05-15 15:12:20 -0500

    Derive key from mnemonic and seed

--- src/signing-tools/methods/wallet.ts
+++ src/signing-tools/methods/wallet.ts
@@ -1,6 +1,8 @@
 import { Network, PrivateKey } from "../../core/types/types";
 import { KeyPair, keyPairFromPrivateKey } from "@nodefactory/filecoin-address";
-import { generateMnemonic } from "bip39";
+import { generateMnemonic, mnemonicToSeedSync } from "bip39";
+import { fromSeed } from "bip32";
+import { keyPairFromSeed } from "@nodefactory/filecoin-address";
 
 export class WalletTools {
     /**
@@ -24,5 +26,36 @@ export class WalletTools {
      */
     public generateMnemonic(strength = 128): string {
         return generateMnemonic(strength);
+    }
+
+    /**
+     * @notice Get key pair from mnemonic
+     * @param mnemonic The mnemonic to derive the key from
+     * @param path Key derivation path
+     * @param network mainnet or testnet
+     * @returns Key pair
+     */
+    public keyDerive(mnemonic: string, path: string, network: Network = "mainnet"): KeyPair {
+        // Get seed from mnemonic
+        const seed = mnemonicToSeedSync(mnemonic);
+        return this.keyDeriveFromSeed(seed, path, network);
+    }
+
+    /**
+     * @notice Get key pair from seed
+     * @param seed The seed to derive the key from
+     * @param path Key derivation path
+     * @param network mainnet or testnet
+     * @returns Key pair
+     */
+    public keyDeriveFromSeed(seed: Buffer, path: string, network: Network = "mainnet"): KeyPair {
+        // Master Key
+        const masterKey = fromSeed(seed);
+
+        // Derive Child Key
+        const childKey = masterKey.derivePath(path);
+
+        // Recover Keys from Private Key
+        return this.keyRecover(Buffer.from(childKey.privateKey).toString("hex"), network);
     }
 }
